747cbd7ddb6641ba09ef85cb71d66cdfa550d2d3
AliceMeneses <alicesilvademeneses@gmail.com>
2021-03-13 15:45:57 -0300

    Adicionar ícone da página

--- src/components/shared/AppTopBar.vue
+++ src/components/shared/AppTopBar.vue
@@ -8,7 +8,7 @@
         >
 
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
+        <v-icon>mdi-egg-easter</v-icon>
         <v-toolbar-title>Nome da Aplicação</v-toolbar-title>
 
         </v-app-bar>  

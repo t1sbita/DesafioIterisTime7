d80c3526d5858b4a2b6b660857bf3fcc9c8ff462
AliceMeneses <alicesilvademeneses@gmail.com>
2021-03-13 13:44:06 -0300

    Adicionar carrossel

--- src/views/Inicial.vue
+++ src/views/Inicial.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="Inicial">
-    
+    <Carrossel/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Carrossel from '../components/Carrossel.vue'
 
 export default {
   name: 'Inicial',
-  
+  components: {
+    Carrossel
+  }
 }
 </script>

2d660e689200c9f5e4328d9bdbb94471e1a50850
Elias Garcia <elias.sgarcia@gmail.com>
2021-03-13 10:41:54 -0300

    Adicionada AppTopBar

---
+++ src/views/Inicial.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Inicial">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Inicial',
+  
+}
+</script>
---
+++ src/views/VendedoresLista.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+      <h2 class="text-center mb-7 mt-2">Lista de Vendedores</h2>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
